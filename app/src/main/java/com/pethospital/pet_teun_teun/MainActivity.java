8c04bf8bb808444117fed872d91980bb21357481
sungyil <shfur2006@gmail.com>
2019-10-06 01:57:40 +0900

    git testing

--- app/src/main/java/com/pethospital/pet_teun_teun/MainActivity.java
+++ app/src/main/java/com/pethospital/pet_teun_teun/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         fragManager=getSupportFragmentManager();
+        
         boardFrag=new BoardFragment();
         userMainFrag=new UserMainFragment();
 
