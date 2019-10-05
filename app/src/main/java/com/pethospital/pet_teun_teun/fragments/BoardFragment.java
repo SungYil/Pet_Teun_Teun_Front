9d29cd54b7c08ccd6545eefe0a1413986668991d
sungyil <shfur2006@gmail.com>
2019-10-06 00:31:41 +0900

    실제 우리 화면에 들어가는 하단바 추가. 아이콘 색 포함해서 다시 다운

---
+++ app/src/main/java/com/pethospital/pet_teun_teun/fragments/BoardFragment.java
@@ -0,0 +1,19 @@
+package com.pethospital.pet_teun_teun.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.pethospital.pet_teun_teun.R;
+
+public class BoardFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.board_fragment, container, false);
+    }
+
+}
