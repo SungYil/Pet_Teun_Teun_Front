17120aa91278a6934d540e700894a37ec5caf56b
sungyil <shfur2006@gmail.com>
2019-10-05 23:48:42 +0900

    하단 아이템 셀렉터를 위한 코드 진행.
    build.gradle에 추가된
    implementation 'com.google.android.material:material:1.0.0-alpha3'
    이 코드는
    bottomNavigationView 사용하기 위한 dependency 임.
    원래 implementation 'com.android.support:design:27.1.1'
    이거였으나 최신버전 업데이트 이후 저 위의 코드로 바뀜

--- app/src/main/java/com/pethospital/pet_teun_teun/MainActivity.java
+++ app/src/main/java/com/pethospital/pet_teun_teun/MainActivity.java
@@ -1,14 +1,37 @@
 package com.pethospital.pet_teun_teun;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.pethospital.pet_teun_teun.fragments.BoardFragment;
+import com.pethospital.pet_teun_teun.fragments.UserMainFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragManager;
+    private BoardFragment boardFrag;
+    private UserMainFragment userMainFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragManager=getSupportFragmentManager();
+        boardFrag=new BoardFragment();
+        userMainFrag=new UserMainFragment();
+
+        FragmentTransaction transaction=fragManager.beginTransaction();
+        transaction.replace(R.id.frameLayout,userMainFrag).commitAllowingStateLoss();
+
+        BottomNavigationView bottomView=findViewById(R.id.navigationView);
+        bottomView.setOnNavigationItemSelectedListener(new ItemSelectedListener(){
+
+        });
+
     }
 }
