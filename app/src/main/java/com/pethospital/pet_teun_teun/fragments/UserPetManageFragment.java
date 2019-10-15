5b20c981b43e771ed24a2e40f952fc075c5c6c22
sungyil <shfur2006@gmail.com>
2019-10-15 23:27:35 +0900

    메인화면에서 펫 상태확인 버튼 클릭 시 이동하는 이벤트 추가 그에 따른 액티비티 매니페이트에 추가

--- app/src/main/java/com/pethospital/pet_teun_teun/fragments/UserPetManageFragment.java
+++ app/src/main/java/com/pethospital/pet_teun_teun/fragments/UserPetManageFragment.java
@@ -1,21 +1,68 @@
 package com.pethospital.pet_teun_teun.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.pethospital.pet_teun_teun.PetStateViewActivity;
 import com.pethospital.pet_teun_teun.R;
 
 public class UserPetManageFragment extends Fragment {
 
+    private ImageButton stateView;
+    private ImageButton reserConfirm;
+    private ImageButton vaccin;
+    private ImageButton hospital;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        View v= inflater.inflate(R.layout.user_pet_manage_fragment, container, false);
+        stateView=(ImageButton)v.findViewById(R.id.state_view);
+        reserConfirm=(ImageButton)v.findViewById(R.id.reservation_confirm);
+        vaccin=(ImageButton)v.findViewById(R.id.vaccin_confirm);
+        hospital=(ImageButton)v.findViewById(R.id.hospital_match);
 
-        return inflater.inflate(R.layout.user_pet_manage_fragment, container, false);
+        stateView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //Intent intent=new Intent(getActivity(), PetStateViewActivity.class);
+                //startActivity(intent);
+                Log.v("상태확인 버튼 클릭","버튼 클릭 완료");
+
+            }
+        });
+
+        reserConfirm.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getActivity(), PetStateViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        vaccin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getActivity(), PetStateViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        hospital.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getActivity(), PetStateViewActivity.class);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
 }
