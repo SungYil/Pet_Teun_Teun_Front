c017126c54a6ed507dc823a239c8301a4a70dee4
rogal3 <cws0188@gmail.com>
2019-10-18 23:56:52 +0900

    just commit

--- app/src/main/java/com/pethospital/pet_teun_teun/JoinUserActivity.java
+++ app/src/main/java/com/pethospital/pet_teun_teun/JoinUserActivity.java
@@ -1,6 +1,7 @@
 package com.pethospital.pet_teun_teun;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +11,12 @@ public class JoinUserActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_user_activity);
+
+        EditText edit_id = findViewById(R.id.join_user_edit_id);
+        EditText edit_password = findViewById(R.id.join_user_edit_password);
+        EditText edit_date = findViewById(R.id.join_user_edit_date);
+        EditText edit_phone = findViewById(R.id.join_user_edit_phone);
+
+
     }
 }
