d1a9b29fbed56212882e3abacc29d5c9ded6441f
sungyil <shfur2006@gmail.com>
2019-11-04 02:19:42 +0900

    예약 통신코드추가

--- app/src/main/java/com/pethospital/pet_teun_teun/ReserveActivity.java
+++ app/src/main/java/com/pethospital/pet_teun_teun/ReserveActivity.java
@@ -1,10 +1,13 @@
 package com.pethospital.pet_teun_teun;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.pethospital.pet_teun_teun.servers.RequestHttpURLConnection;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,6 +19,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 public class ReserveActivity extends AppCompatActivity {
     private Spinner doctorSpinner;
@@ -38,6 +44,10 @@ public class ReserveActivity extends AppCompatActivity {
         locationText=findViewById(R.id.reserve_hospital_location);
         phoneNum=findViewById(R.id.reserve_hospital_tel);
         backBtn=findViewById(R.id.reserve_back_button);
+
+        reserveText.setText(intent.getStringExtra("name"));
+        locationText.setText(intent.getStringExtra("location"));
+        phoneNum.setText(intent.getStringExtra("phone"));
 
         String[] str=getResources().getStringArray(R.array.doctor_list);
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.spinner_doctor,str);
@@ -62,6 +72,63 @@ public class ReserveActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+    }
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //progress bar를 보여주는 등등의 행위
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result; // 요청 결과를 저장할 변수.
+            com.pethospital.pet_teun_teun.servers.RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection(getApplicationContext());
+            result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+            Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+
+            try {
+                JSONObject json = new JSONObject(s);
+                if("ok".equals(json.getString("msg"))){
+
+                    if("hospital".equals(json.getString("user"))){
+                        intent.putExtra("type","hospital");
+                        startActivity(intent);
+                    }else if("user".equals(json.getString("user"))){
+                        intent.putExtra("type","user");
+                        startActivity(intent);
+                    }else{
+
+                    }
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_LONG).show();
+                }
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
 }
