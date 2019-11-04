bde9d2e3eae62d1021cc5371a32c5a78d7547158
lee <jr30jr@naver.com>
2019-11-05 03:11:06 +0900

    예약 취소 제작중

--- app/src/main/java/com/pethospital/pet_teun_teun/DetailPostActivity.java
+++ app/src/main/java/com/pethospital/pet_teun_teun/DetailPostActivity.java
@@ -1,37 +1,110 @@
 package com.pethospital.pet_teun_teun;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.pethospital.pet_teun_teun.adapters.CommentAdapter;
 import com.pethospital.pet_teun_teun.items.Comment;
+import com.pethospital.pet_teun_teun.servers.RequestHttpURLConnection;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class DetailPostActivity extends AppCompatActivity {
     private ListView listView;
     private CommentAdapter commentAdapter;
+    private TextView titleTv;
+    private TextView writerNicNameTv;
+    private TextView dateTv;
+    private TextView contentsTv;
+    private TextView commentCntTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_post);
-
+        setting();
+        Intent intent=getIntent();
+        String postID=intent.getStringExtra("postID");
+        Toast.makeText(getApplicationContext(),postID,Toast.LENGTH_SHORT).show();
+        new NetworkProcess(getApplicationContext(),postID).execute();
+    }
+    void setting(){
         listView= findViewById(R.id.detail_comment_list_view);
         commentAdapter=new CommentAdapter();
-
         listView.setAdapter(commentAdapter);
-
+        titleTv=(TextView)findViewById(R.id.detail_post_title);
+        writerNicNameTv=(TextView)findViewById(R.id.detail_post_writer_nickname);
+        contentsTv=(TextView)findViewById(R.id.detail_post_contents_area);
+        dateTv=(TextView)findViewById(R.id.detail_post_write_date);
+        commentCntTv=(TextView)findViewById(R.id.detail_post_comment_cnt);
+    }
+    void addData(){
         ArrayList<Comment> sample=new SampleMange().getCommentList();
         for(int i=0;i<sample.size();i++){
             commentAdapter.addItem(sample.get(i));
         }
     }
+    private class NetworkProcess extends AsyncTask<Void,Void,String> {
+        private Context context;
+        private String postID;
+        public NetworkProcess(Context context,String postID) {
+            this.context=context;
+            this.postID=postID;
+        }
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String result=null;
+            RequestHttpURLConnection requestHttpURLConnection=new RequestHttpURLConnection(context);
+            String url=getString(R.string.url)+"getPostDetail.do";
+            //Toast.makeText(context,url,Toast.LENGTH_SHORT).show();
+            ContentValues values=new ContentValues();
+            values.put("postID",postID);
+            result=requestHttpURLConnection.request(url,values);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(context,s,Toast.LENGTH_SHORT).show();
+            try{
+                JSONObject jsonObject=new JSONObject(s);
+                JSONArray ary=jsonObject.getJSONArray("comments");
+                titleTv.setText(jsonObject.getString("title"));
+                writerNicNameTv.setText(jsonObject.getString("writerNickname"));
+                dateTv.setText(jsonObject.getString("date"));
+                contentsTv.setText(jsonObject.getString("contents"));
+                commentCntTv.setText(jsonObject.getString("commentCnt"));
+                for(int i=0;i<ary.length();i++){
+                    JSONObject object=ary.getJSONObject(i);
+                    Toast.makeText(context,object.toString(),Toast.LENGTH_SHORT).show();
+                }
+            }
+            catch(Exception e) {
+                Toast.makeText(context,e.toString(),Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
--- app/src/main/java/com/pethospital/pet_teun_teun/fragments/BoardPageFragment.java
+++ app/src/main/java/com/pethospital/pet_teun_teun/fragments/BoardPageFragment.java
@@ -1,20 +1,35 @@
 package com.pethospital.pet_teun_teun.fragments;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.pethospital.pet_teun_teun.AddPostActivity;
+import com.pethospital.pet_teun_teun.DetailPostActivity;
+import com.pethospital.pet_teun_teun.MainActivity;
 import com.pethospital.pet_teun_teun.R;
 import com.pethospital.pet_teun_teun.SampleMange;
+import com.pethospital.pet_teun_teun.SearchPostActivity;
 import com.pethospital.pet_teun_teun.adapters.PostAdapter;
 import com.pethospital.pet_teun_teun.items.Post;
+import com.pethospital.pet_teun_teun.servers.RequestHttpURLConnection;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -29,7 +44,8 @@ public class BoardPageFragment extends Fragment {
         view=inflater.inflate(R.layout.board_page, container, false);
 
         setting();
-        addData();
+        setEvent();
+        new NetworkProcess(view.getContext()).execute();
         return view;
     }
     void setting(){
@@ -41,13 +57,88 @@ public class BoardPageFragment extends Fragment {
         addPostBtn=view.findViewById(R.id.board_page_add_post_btn);
         //내 게시글 보기버튼
         myPostBtn=view.findViewById(R.id.board_page_my_btn);
+        //게시글 검색버튼
+        searchBtn=view.findViewById(R.id.board_page_search_btn);
     }
-    void addData(){
-        SampleMange sample=new SampleMange();
-        ArrayList<Post> temp=sample.getPostList();
+    void setEvent(){
+        addPostBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent addPost=new Intent(getContext(), AddPostActivity.class);
+                startActivity(addPost);
+            }
+        });
+        myPostBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getContext(), SearchPostActivity.class);
+                startActivity(intent);
+            }
+        });
+        searchBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getContext(), SearchPostActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        for(int i=0;i<temp.size();i++){
-            postAdapter.addItem(temp.get(i));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Post item=(Post)postAdapter.getItem(i);
+                //Toast.makeText(view.getContext(), item.getId(), Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(view.getContext(), DetailPostActivity.class);
+                intent.putExtra("postID",item.getId());
+                startActivity(intent);
+            }
+        });
+    }
+
+    private class NetworkProcess extends AsyncTask<Void,Void,String>{
+        private Context context;
+        public NetworkProcess(Context context) {
+            this.context=context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String result=null;
+            RequestHttpURLConnection requestHttpURLConnection=new RequestHttpURLConnection(getContext());
+            String url=getString(R.string.url)+"getPostList.do";
+            //Toast.makeText(context,url,Toast.LENGTH_SHORT).show();
+            ContentValues values=new ContentValues();
+            String id= getContext().getSharedPreferences("login", Context.MODE_PRIVATE).getString("id","");
+            values.put("memberID",id);
+            result=requestHttpURLConnection.request(url,values);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try{
+                JSONObject jsonObject=new JSONObject(s);
+                JSONArray ary=jsonObject.getJSONArray("list");
+                for(int i=0;i<ary.length();i++){
+                    JSONObject object=ary.getJSONObject(i);
+                    Toast.makeText(context,object.toString(),Toast.LENGTH_SHORT).show();
+                    Post post=new Post();
+                    //String id, int index, String title, String writerNickname, String date, int viewCnt, int commentCnt, String imgUrl
+                    postAdapter.addItem(new Post(object.getString("id"),0,object.getString("title"),object.getString("writerNickname"),object.getString("date"),Integer.parseInt(object.getString("viewCnt")),Integer.parseInt(object.getString("commentCnt")),object.getString("imgUrl")));
+                    postAdapter.notifyDataSetChanged();
+                }
+            }
+            catch(Exception e){
+
+            }
+
+            //Toast.makeText(context,s,Toast.LENGTH_SHORT).show();
         }
     }
 }
