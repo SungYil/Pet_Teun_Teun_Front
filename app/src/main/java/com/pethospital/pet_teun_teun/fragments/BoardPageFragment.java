926ac5a1d4e4bfba9afbba9e1b14fb23a97f9e62
lee <jr30jr@naver.com>
2019-11-04 21:27:59 +0900

    게시글 화면 수정,게시글 등록화면 제작중

--- app/src/main/java/com/pethospital/pet_teun_teun/fragments/BoardPageFragment.java
+++ app/src/main/java/com/pethospital/pet_teun_teun/fragments/BoardPageFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -20,15 +21,26 @@ import java.util.ArrayList;
 public class BoardPageFragment extends Fragment {
     private ListView listView;
     private PostAdapter postAdapter;
-
+    private ImageButton addPostBtn;
+    private ImageButton searchBtn;
+    private ImageButton myPostBtn;
+    private View view;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v=inflater.inflate(R.layout.board_page, container, false);
-        listView= v.findViewById(R.id.board_post_list_view);
+        view=inflater.inflate(R.layout.board_page, container, false);
+
+        setting();
+        addData();
+        return view;
+    }
+    void setting(){
+        listView= view.findViewById(R.id.board_post_list_view);
         postAdapter=new PostAdapter();
         listView.setAdapter(postAdapter);
 
-        addData();
-        return v;
+        //게시글 등록버튼
+        addPostBtn=view.findViewById(R.id.board_page_add_post_btn);
+        //내 게시글 보기버튼
+        myPostBtn=view.findViewById(R.id.board_page_my_btn);
     }
     void addData(){
         SampleMange sample=new SampleMange();
