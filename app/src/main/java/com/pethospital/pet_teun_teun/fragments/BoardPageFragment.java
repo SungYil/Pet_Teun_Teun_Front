654e0b784c65b189bfe74bc8713fc31e1550d0d9
lee <jr30jr@naver.com>
2019-11-04 21:22:02 +0900

    예약 취소 제작중

--- app/src/main/java/com/pethospital/pet_teun_teun/fragments/BoardPageFragment.java
+++ app/src/main/java/com/pethospital/pet_teun_teun/fragments/BoardPageFragment.java
@@ -4,15 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.pethospital.pet_teun_teun.R;
+import com.pethospital.pet_teun_teun.SampleMange;
+import com.pethospital.pet_teun_teun.adapters.PostAdapter;
+import com.pethospital.pet_teun_teun.items.Post;
+
+import java.util.ArrayList;
 
 public class BoardPageFragment extends Fragment {
+    private ListView listView;
+    private PostAdapter postAdapter;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.board_page, container, false);
+        View v=inflater.inflate(R.layout.board_page, container, false);
+        listView= v.findViewById(R.id.board_post_list_view);
+        postAdapter=new PostAdapter();
+        listView.setAdapter(postAdapter);
+
+        addData();
+        return v;
+    }
+    void addData(){
+        SampleMange sample=new SampleMange();
+        ArrayList<Post> temp=sample.getPostList();
+
+        for(int i=0;i<temp.size();i++){
+            postAdapter.addItem(temp.get(i));
+        }
     }
 }
